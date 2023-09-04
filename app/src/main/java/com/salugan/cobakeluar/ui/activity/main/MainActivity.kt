d619f3712e8a9c1a0a546ce43c7f4e0bc221393c
Julio Nicholas <juliojnicolas1@gmail.com>
2023-09-04 13:17:00 +0700

    slicing ui

--- app/src/main/java/com/salugan/cobakeluar/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/salugan/cobakeluar/ui/activity/main/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.salugan.cobakeluar.ui.activity.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.salugan.cobakeluar.R
+import com.salugan.cobakeluar.databinding.ActivityMainBinding
+import com.salugan.cobakeluar.ui.activity.materi.MateriScreenActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MateriScreenActivity::class.java))
+        }
     }
 }
