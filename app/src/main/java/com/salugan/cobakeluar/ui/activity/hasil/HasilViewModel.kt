bfce40f55f491b593bc1a887de60f322975156f3
faiz <faizivntm@gmail.com>
2023-09-12 13:41:02 +0700

    hasil View Model

---
+++ app/src/main/java/com/salugan/cobakeluar/ui/activity/hasil/HasilViewModel.kt
@@ -0,0 +1,10 @@
+package com.salugan.cobakeluar.ui.activity.hasil
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HasilViewModel @Inject constructor(
+
+) {
+}
