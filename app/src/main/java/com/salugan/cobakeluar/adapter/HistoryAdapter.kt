0896d614fc898bb86f901af04f0e6dfbc3bdaf29
Julio Nicholas <juliojnicolas1@gmail.com>
2023-09-14 09:37:50 +0700

    Add “benar”, “salah”, “kosong” label for the tryout history item.

--- app/src/main/java/com/salugan/cobakeluar/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/salugan/cobakeluar/adapter/HistoryAdapter.kt
@@ -3,6 +3,7 @@ package com.salugan.cobakeluar.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.salugan.cobakeluar.R
 import com.salugan.cobakeluar.databinding.ItemCardBinding
 import com.salugan.cobakeluar.model.HasilModel
 
@@ -24,8 +25,9 @@ class HistoryAdapter(private val hasilHistory: ArrayList<HasilModel>)
 
     class ViewHolder(private val binding: ItemCardBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(hasilModel: HasilModel) {
-            binding.benar.text = hasilModel.benar.toString()
-            binding.salah.text = hasilModel.salah.toString()
+            binding.benar.text = String.format(itemView.context.getString(R.string.benar), hasilModel.benar)
+            binding.salah.text = String.format(itemView.context.getString(R.string.salah), hasilModel.salah)
+            binding.kosong.text = String.format(itemView.context.getString(R.string.kosong), hasilModel.kosong)
             binding.tanggal.text = hasilModel.tanggal
             binding.waktu.text = hasilModel.waktu
             binding.nilaiData.text = hasilModel.nilai
