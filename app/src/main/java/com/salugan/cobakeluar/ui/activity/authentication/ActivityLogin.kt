c8d5772f24925c7784a1e00e6f55a4afaa9a1e64
faiz <faizivntm@gmail.com>
2023-09-15 09:31:31 +0700

    auth documentation

--- app/src/main/java/com/salugan/cobakeluar/ui/activity/authentication/ActivityLogin.kt
+++ app/src/main/java/com/salugan/cobakeluar/ui/activity/authentication/ActivityLogin.kt
@@ -28,26 +28,47 @@ class ActivityLogin: AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         mAuth = FirebaseAuth.getInstance()
-        binding.btnMasuk.setOnClickListener({
-            val email = binding.txtEmail.text.toString()
-            val password = binding.txtPassword.text.toString()
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                loginUser(email, password)
-            } else {
-                Toast.makeText(this, "Email dan password harus di isi", Toast.LENGTH_SHORT).show()
-            }
-        })
-//        binding.btnDaftar.setOnClickListener({
-//            val intent = Intent(this, ActivitySignUp::class.java)
-//            startActivity(intent)
-//        })
-        binding.btnLoginGoogle.setOnClickListener({
+        binding.btnMasuk.setOnClickListener {
+            validasiLogin()
+        }
+
+        binding.btnLoginGoogle.setOnClickListener {
             signIn()
-        })
-        binding.btnTextLupaPassword.setOnClickListener({
+        }
+        binding.btnTextLupaPassword.setOnClickListener {
             forgotPassword()
-        })
+        }
     }
+
+    /**
+     * This function validates the user's login input by checking if both the email and password fields are not empty.
+     * If both fields are filled, it proceeds to log in the user by calling the `loginUser` function with the provided email
+     * and password. If either the email or password field is empty, it displays a toast message to prompt the user to
+     * enter both their email and password.
+     * @author [Faiz Ivan Tama]
+     * @since September 2023.
+     * @see loginUser
+     */
+    private fun validasiLogin(){
+        val email = binding.txtEmail.text.toString()
+        val password = binding.txtPassword.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            loginUser(email, password)
+        } else {
+            Toast.makeText(this, "Email dan password harus di isi", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    /**
+     * This function is used to authenticate a user with their email and password
+     *
+     * @param email The email address of the user.
+     * @param password The password of the user.
+     * @author [Faiz Ivan Tama]
+     * @since September 2023.
+     * @see [Firebase Authentication documentation](https://firebase.google.com/docs/auth)
+     */
     private fun loginUser(email: String, password: String) {
         loading()
         mAuth.signInWithEmailAndPassword(email, password)
@@ -63,6 +84,15 @@ class ActivityLogin: AppCompatActivity() {
                 }
             }
     }
+
+    /*
+    *This function is used to initiate Google Sign-In for the user.
+    * It sets up the necessary Google Sign-In options, client, and starts the Google Sign-In process.
+    *
+    * @author [Faiz Ivan Tama]
+    * @since September 2023.
+    * @see [Google Sign-In documentation](https://developers.google.com/identity/sign-in/android/start-integrating)
+    */
     private fun signIn() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -72,6 +102,18 @@ class ActivityLogin: AppCompatActivity() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, requestCode)
     }
+
+    /**
+     * This function is called when an activity that was started with [startActivityForResult]
+     * finishes and returns a result. It is used to handle the result of the Google Sign-In process.
+     *
+     * @param requestCode The request code passed to [startActivityForResult].
+     * @param resultCode The result code returned by the child activity.
+     * @param data An [Intent] that carries the result data from the child activity.
+     * @author [Faiz Ivan Tama]
+     * @since September 2023.
+     * @see [Google Sign-In documentation](https://developers.google.com/identity/sign-in/android/sign-in)
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == requestCode && resultCode == RESULT_OK) {
@@ -85,6 +127,16 @@ class ActivityLogin: AppCompatActivity() {
             }
         }
     }
+
+    /**
+     * This function is used to authenticate a user with Google Sign-In credentials.
+     * It takes a GoogleSignInAccount as input and uses its ID token to sign in the user.
+     *
+     * @param account The GoogleSignInAccount containing user information and credentials.
+     * @author [Faiz Ivan Tama]
+     * @since September 2023.
+     * @see [Google Sign-In documentation](https://developers.google.com/identity/sign-in/android/sign-in)
+     */
     private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
         loading()
         val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
@@ -129,10 +181,26 @@ class ActivityLogin: AppCompatActivity() {
                 }
             }
     }
+
+
+    /**
+     * This function is used to going to forgot password screen
+     *
+     * @author [Faiz Ivan Tama]
+     * @since September 2023.
+     * @see ActivityForgotPassword
+     */
     private fun forgotPassword() {
         val intent = Intent(this, ActivityForgotPassword::class.java)
         startActivity(intent)
     }
+
+
+    /**
+     * Handle process is loading
+     * @author [Faiz Ivan Tama]
+     * @since September 2023.
+     * */
     private fun loading() {
         val dialogView = layoutInflater.inflate(R.layout.dialog_loading, null)
         val builder = AlertDialog.Builder(this)
